f49209b277340f6b19bf0e62e29875ffad324b5f
JoshG612 <joshgreene0612@gmail.com>
2014-12-19 12:24:44 -0700

    Update

---
+++ BridgeofDeathv2/Form1.Designer.vb
@@ -0,0 +1,95 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnapproach = New System.Windows.Forms.Button()
+        Me.lblfate = New System.Windows.Forms.Label()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.lblQuest = New System.Windows.Forms.Label()
+        Me.lblRandom = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnapproach
+        '
+        Me.btnapproach.Location = New System.Drawing.Point(59, 23)
+        Me.btnapproach.Name = "btnapproach"
+        Me.btnapproach.Size = New System.Drawing.Size(162, 27)
+        Me.btnapproach.TabIndex = 0
+        Me.btnapproach.Text = "Approach the Bridge"
+        Me.btnapproach.UseVisualStyleBackColor = True
+        '
+        'lblfate
+        '
+        Me.lblfate.AutoSize = True
+        Me.lblfate.Location = New System.Drawing.Point(120, 227)
+        Me.lblfate.Name = "lblfate"
+        Me.lblfate.Size = New System.Drawing.Size(0, 13)
+        Me.lblfate.TabIndex = 1
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.Location = New System.Drawing.Point(29, 147)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(0, 13)
+        Me.lblName.TabIndex = 2
+        '
+        'lblQuest
+        '
+        Me.lblQuest.AutoSize = True
+        Me.lblQuest.Location = New System.Drawing.Point(123, 146)
+        Me.lblQuest.Name = "lblQuest"
+        Me.lblQuest.Size = New System.Drawing.Size(0, 13)
+        Me.lblQuest.TabIndex = 3
+        '
+        'lblRandom
+        '
+        Me.lblRandom.AutoSize = True
+        Me.lblRandom.Location = New System.Drawing.Point(215, 146)
+        Me.lblRandom.Name = "lblRandom"
+        Me.lblRandom.Size = New System.Drawing.Size(0, 13)
+        Me.lblRandom.TabIndex = 4
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.lblRandom)
+        Me.Controls.Add(Me.lblQuest)
+        Me.Controls.Add(Me.lblName)
+        Me.Controls.Add(Me.lblfate)
+        Me.Controls.Add(Me.btnapproach)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnapproach As System.Windows.Forms.Button
+    Friend WithEvents lblfate As System.Windows.Forms.Label
+    Friend WithEvents lblName As System.Windows.Forms.Label
+    Friend WithEvents lblQuest As System.Windows.Forms.Label
+    Friend WithEvents lblRandom As System.Windows.Forms.Label
+
+End Class
---
+++ BridgeofDeathv2/Form1.vb
@@ -0,0 +1,75 @@
+﻿Public Class Form1
+
+    'Code for Input Boxes
+    Private Sub btnapproach_Click(sender As Object, e As EventArgs) Handles btnapproach.Click
+        Dim strName As String = InputBox("what is your name?", "Name")
+        Dim strQuest As String = InputBox("what is your quest?", "Quest")
+        Dim strRandomQAnwer As String
+        Dim intRandomQuestionNumber As Integer
+
+
+
+        'Random Questions
+        Dim strRandomQ1 As String = "What is your favorite color?"
+        Dim strRandomQ2 As String = "What is the flight speed of an unladen swallow?"
+        Dim strRandomQ3 As String = "What is the capital of Assyria?"
+        Dim strRandomQ4 As String = "What is the depth of the polar ice cap?"
+
+
+        'Responses
+        Dim strRespnse As String
+
+        'Random Question Answers
+        Dim strRandomQA As String
+        Dim strRandomQA1 As String = "Blue"
+        Dim strRandomQA2 As String = "10 mph"
+        Dim strRandomQA3 As String = "Assur"
+        Dim strRandomQA4 As String = "500 ft"
+
+        'typical random number sequence
+        Randomize()
+        intRandomQuestionNumber = Int((4 - 1 + 1) * Rnd() + 1)
+
+        If intRandomQuestionNumber = 1 Then
+            strRandomQAnwer = InputBox(strRandomQ1, "Random")
+            strRandomQA = strRandomQA1
+        ElseIf intRandomQuestionNumber = 2 Then
+            strRandomQAnwer = InputBox(strRandomQ2, "Random")
+            strRandomQA = strRandomQA2
+        ElseIf intRandomQuestionNumber = 3 Then
+            strRandomQAnwer = InputBox(strRandomQ3, "Random")
+            strRandomQA = strRandomQA3
+        ElseIf intRandomQuestionNumber = 4 Then
+            strRandomQAnwer = InputBox(strRandomQ4, "Random")
+            strRandomQA = strRandomQA4
+        End If
+
+
+        If strName = "Josh" Then
+            Me.lblName.Text = "True"
+        Else
+            Me.lblName.Text = "False"
+        End If
+
+        If strQuest = "Seek the grail" Then
+            Me.lblQuest.Text = "True"
+        Else
+            Me.lblQuest.Text = "False"
+        End If
+        If strRandomQAnwer = strRandomQA Then
+            Me.lblRandom.Text = "True"
+        Else
+            Me.lblRandom.Text = "False"
+            If strRandomQAnwer = "Nineveh" And intRandomQuestionNumber = 3 Then
+                Me.lblRandom.Text = "True"
+            End If
+        End If
+
+        If Me.lblQuest.Text = "True" And Me.lblName.Text = "True" And Me.lblRandom.Text = "True" Then
+            Me.lblfate.Text = "You may pass"
+        Else
+            Me.lblfate.Text = "You will die"
+        End If
+
+    End Sub
+End Class
